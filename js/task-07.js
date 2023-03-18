845cf5582ac1972fe9eca8b952d6ee4ab18f3d42
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-03-18 21:05:31 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,11 @@
-const inputEl = document.querySelector("input");
-const spanEl = document.querySelector("span");
-inputEl.addEventListener("input", onChangeInput);
-function onChangeInput() {
+// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size.
+// В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
+const inputEl = document.querySelector("#font-size-control");
+const spanEl = document.querySelector("#text");
+
+inputEl.addEventListener("input", onChangeFontSizeInput);
+
+function onChangeFontSizeInput() {
   spanEl.style.fontSize = `${inputEl.value}px`;
 }
